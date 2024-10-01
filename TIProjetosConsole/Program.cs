956d238c2d185942c8395073a8e15fa1c332240b
EDUARDO VINICIUS FALICO DA COSTA <eduardo.vfcosta@senacsp.edu.br>
2024-09-30 22:20:09 -0300

    Meu primeiro codigo

--- TIProjetosConsole/Program.cs
+++ TIProjetosConsole/Program.cs
@@ -10,8 +10,32 @@ namespace TIProjetosConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ola Mundo");
-            Console.WriteLine("Teste, linha 2");
+            string Nome;
+            decimal nota1, nota2, nota3, nota4, resultado;
+
+            Console.WriteLine("Digite seu Nome, " );
+            Nome=Console.ReadLine();
+            Console.WriteLine("Seja Bem Vindo ",Nome);
+
+            //Entre com notas
+
+
+            Console.WriteLine("Digite as Notas do aluno ",Nome);
+            nota1= decimal.Parse(Console.ReadLine());
+            nota2 = decimal.Parse(Console.ReadLine());
+            nota3 = decimal.Parse(Console.ReadLine());  
+            nota4 = decimal.Parse(Console.ReadLine());  
+            
+            //processmento
+
+            resultado =(nota1 + nota2+nota3+nota4)/4;
+
+            //saida
+
+            Console.WriteLine("O Aluno teve media final " + resultado);
+
+            Console.ReadKey();
+
         }
     }
 }
